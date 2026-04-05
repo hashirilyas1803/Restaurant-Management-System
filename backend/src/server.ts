2d3d5c2eb9c6e045dfcd2ee817f3991f6918dd70
Muhammad Hashir Ilyas <muhammadhashir.ilyas@gmail.com>
2026-04-05 10:52:36 +0500

    Implement Dish Management

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import reservationRoutes from './routes/reservationRoutes';
 import authRoutes from './routes/authRoutes';
 import orderRoutes from './routes/orderRoutes';
+import cateringRoutes from './routes/cateringRoutes';
+import dishRoutes from './routes/dishRoutes';
 
 // Create the express app
 const app = express();
@@ -16,11 +18,17 @@ app.use(express.json());
 // Mount the authentication routes
 app.use('/api/auth', authRoutes);
 
+// Mount the dish routes
+app.use('/api/dishes', dishRoutes);
+
 // Mount the reservation routes
 app.use('/api/reservations', reservationRoutes);
 
 // Mount the order routes
 app.use('/api/orders', orderRoutes);
+
+// Mount the catering routes
+app.use('/api/caterings', cateringRoutes);
 
 // Set the app to listen on port 3000
 app.listen(3000, () => {
